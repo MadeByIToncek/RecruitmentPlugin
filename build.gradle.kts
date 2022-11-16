334d4af40d793ea9f81e7950d3c35dea8c3995a8
Made By IToncek <toncek2019@post.cz>
2022-11-16 19:10:33 +0100

    Gradle 🐘

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,16 +3,18 @@ plugins {
 }
 
 group = "pro.rajce"
-version = "1.0"
+version = "1.1"
 
 repositories {
     mavenCentral()
     maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
     maven("https://oss.sonatype.org/content/repositories/snapshots")
+    maven("https://repo.extendedclip.com/content/repositories/placeholderapi/")
 }
 
 dependencies {
     compileOnly("org.spigotmc:spigot-api:1.19.2-R0.1-SNAPSHOT")
+    compileOnly("me.clip:placeholderapi:2.11.2")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.0")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.0")
 }
@@ -20,3 +22,4 @@ dependencies {
 tasks.getByName<Test>("test") {
     useJUnitPlatform()
 }
+
