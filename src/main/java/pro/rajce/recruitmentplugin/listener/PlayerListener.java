167ea22b950c07a4b0b0efd5f1137cea26886e44
Made By IToncek <toncek2019@post.cz>
2022-11-16 20:28:46 +0100

    Gradle 🐘

--- src/main/java/pro/rajce/recruitmentplugin/listener/PlayerListener.java
+++ src/main/java/pro/rajce/recruitmentplugin/listener/PlayerListener.java
@@ -4,6 +4,12 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerBedEnterEvent;
 import org.bukkit.event.player.PlayerBedLeaveEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+import java.util.Objects;
+import java.util.Random;
+
+import static pro.rajce.recruitmentplugin.RecruitmentPlugin.config;
 
 /**
  * Listener for player events
@@ -16,6 +22,7 @@ public class PlayerListener implements Listener {
     public void onBedEnter(PlayerBedEnterEvent event) {
         event.getPlayer().sendMessage("Chceš jít spát? Dobře tedy.");
     }
+    
     /**
      * Triggers when bed is left
      */
@@ -23,6 +30,13 @@ public class PlayerListener implements Listener {
     public void onBedLeave(PlayerBedLeaveEvent event) {
         event.getPlayer().sendMessage("Snad se ti hezky leželo");
     }
-
-
+    
+    /**
+     * Triggers when player joins the server
+     */
+    @EventHandler(ignoreCancelled = true)
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        Random random = new Random();
+        event.setJoinMessage((String) Objects.requireNonNull(config.getList("welcome")).get(random.nextInt(Objects.requireNonNull(config.getList("welcome")).size())));
+    }
 }
